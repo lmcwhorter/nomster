c0762a0408a1ecf4fe56b1fbdeafd03783320e43
Laura McWhorter <lmcwhorter@gmail.com>
2017-02-13 17:30:27 +0000

    Added pagination for places but CSS styling not working

--- Gemfile
+++ Gemfile
@@ -48,3 +48,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+
+# Gemfile to add latest version of will_paginate
+gem 'will_paginate'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+#    @places = Place.all Swapped out for the line below to introduce pagination
+    @places = Place.paginate(:page => params[:page], :per_page => 10)
   end
 
 end
