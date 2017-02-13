20ebd7f3034a63ae1d9597b55c7e5c862bda7d1e
Laura McWhorter <lmcwhorter@gmail.com>
2017-02-13 17:59:07 +0000

    Creating place form, undefined method error in places/new.html.erb, line 3

--- Gemfile
+++ Gemfile
@@ -49,5 +49,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
 
+gem 'simple_form'
+
 # Gemfile to add latest version of will_paginate
 gem 'will_paginate'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,11 @@
 class PlacesController < ApplicationController
 
   def index
-#    @places = Place.all Swapped out for the line below to introduce pagination
     @places = Place.paginate(:page => params[:page], :per_page => 10)
+  end
+
+  def new
+    @place = Place.new
   end
 
 end
