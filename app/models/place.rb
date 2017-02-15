175bca373d546d0374162de8ffc1b0508e9a6388
Laura McWhorter <lmcwhorter@gmail.com>
2017-02-15 21:21:24 +0000

    Added validations for address & description fields

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,12 @@
-class Place < ApplicationRecord
+# class Place < ApplicationRecord
+#  belongs_to :user
+#  validates :name, presence: true
+#end
+
+# Changed to validate all three fields
+class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, :length => { minimum:3 },:presence => true
+  validates :address, :length => { minimum: 6 },:presence => true
+  validates :description, :presence => true
 end
